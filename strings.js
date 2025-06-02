d19729021fdb4dcb9d66209df4854b6e56347774
Zabadoooj <qastuw@gmail.com>
2025-06-02 11:59:47 +0300

    qweq

---
+++ strings.js
@@ -0,0 +1,21 @@
+// const normalizeSpaces = (str) => {
+  // return str.trim().replace(/\s+/g, ' ');
+// };
+// 
+// console.log(normalizeSpaces('  Hello   world! '));
+
+
+// const extractFileName = (catalog) => {
+//   const catalog_arr = catalog.split(['/'])
+//   const result = catalog_arr[catalog_arr.length - 1].split('.')
+//   return result[0]
+// };
+
+// console.log(extractFileName('C:/Users/username/Documents/example.txt'));
+// console.log(extractFileName('../index.html'));
+// console.log(extractFileName('/home/user/images/photo.jpg'));
+
+
+const ecryptSentence = (setnence) => {
+  
+};
